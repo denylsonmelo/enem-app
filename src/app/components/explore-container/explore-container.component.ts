cad4094e3824b40314a5ef267f93d4e85be7174b
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2023-05-17 19:02:54 -0300

    construindo tela de perfil

--- src/app/components/explore-container/explore-container.component.ts
+++ src/app/components/explore-container/explore-container.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { DetalhamentoAlunoComponent } from '../components/detalhamento-aluno/detalhamento-aluno.component';
-import { Aluno } from '../models/aluno';
+import { Aluno } from '../../models/aluno';
+import { DetalhamentoAlunoComponent } from '../detalhamento-aluno/detalhamento-aluno.component';
 
 @Component({
   selector: 'app-explore-container',
--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -3,34 +3,45 @@ import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'abas',
     component: TabsPage,
     children: [
       {
+        //abas/tab1
         path: 'tab1',
         loadComponent: () =>
           import('../tab1/tab1.page').then((m) => m.Tab1Page),
       },
       {
+        //abas/tab2
         path: 'tab2',
         loadComponent: () =>
           import('../tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
+        //abas/tab3
         path: 'tab3',
         loadComponent: () =>
           import('../tab3/tab3.page').then((m) => m.Tab3Page),
       },
       {
+        //abas/perfil
+        path: 'perfil',
+        loadComponent: () =>
+          import('../perfil/perfil.page').then((m) => m.PerfilPage),
+      },
+      {
+        //abas/
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/abas/perfil',
         pathMatch: 'full',
       },
     ],
   },
   {
+    // /
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/abas/perfil',
     pathMatch: 'full',
   },
 ];
