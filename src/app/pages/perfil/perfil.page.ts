b5506e1d2344bf3c8c36d4cf4164131665d1f1a8
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2023-05-18 20:00:08 -0300

    adicionando carregamento de tela baseado no segmento selecionado

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -11,12 +11,29 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule],
 })
 export class PerfilPage implements OnInit {
+  areaSelecionada = 'conquistas';
+
   usuario = {
     nome: 'Nome do Usuário',
     imagemUrl: 'assets/perfis/mulher01.svg',
+    quests: [
+      {
+        nome: 'quest 01',
+        objetivo: 'objetivo 01',
+      },
+      {
+        nome: 'quest 02',
+        objetivo: 'objetivo 02',
+      },
+    ],
   };
 
   constructor() {}
 
   ngOnInit() {}
+
+  trocaAreaSelecionada(evento: any) {
+    console.log(`evento: ${evento.detail.value}`);
+    console.log(`area selecionada: ${this.areaSelecionada}`);
+  }
 }
